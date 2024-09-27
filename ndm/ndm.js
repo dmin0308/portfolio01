27d94c7fc3ef09a3e65c0aaf3005d5c3443cca41
dmin0308 <sonja4485@gmail.com>
2024-09-27 10:58:18 +0900

    for of 구문으로 변경 새탭으로 열기 포함

--- ndm/ndm.js
+++ ndm/ndm.js
@@ -1,20 +1,13 @@
-// window.addEventListener('load', function(){
-
-//   const mynavi = 
-//   this.document.querySelectorAll("#navmenu li"); //배열형 데이터
-
-//   mynavi[0].innerHTML = `<a href="${navidata[0].d1href}">${navidata[0].d1text}</a>`,
-//   mynavi[1].innerHTML = `<a href="${navidata[1].d1href}">${navidata[1].d1text}</a>`;
-
-// })
-
-
 window.addEventListener('load', function() {
 
-  const mynavi = this.document.querySelectorAll("#navmenu li"); // 배열형 데이터
+  const mynavi = this.document.querySelector("#navmenu ul"); // 배열형 데이터
 
-  for (let x in mynavi) {
-    mynavi[x].innerHTML = `<a href="${navidata[x].d1href}">${navidata[x].d1text}</a>`;
+  let mytag = '';
+
+
+  for( x of navidata){
+    mytag += `<li><a href="${x.d1href}" target="${x.__target}">${x.d1text}</a></li>`
   }
 
+  mynavi.innerHTML = mytag;
 });
