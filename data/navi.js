be0ca1d4beff16ff71a316ff93889121f677e0b2
dmin0308 <sonja4485@gmail.com>
2024-10-15 18:07:41 +0900

    viemo hero section

--- data/navi.js
+++ data/navi.js
@@ -2,17 +2,17 @@
 const navidata = [
   {
     d1text : "Brand",
-    d1href : "about.html",
+    d1href : "",
     __target : "_blank"
   },
   {
     d1text : "Our Services",
-    d1href : "services.html",
+    d1href : "#services",
     __target : ""
   },
   {
     d1text : "Product",
-    d1href : "services.html",
+    d1href : "",
     __target : ""
   },
   {
@@ -22,7 +22,7 @@ const navidata = [
   },
   {
     d1text : "Contact us",
-    d1href : "services.html",
+    d1href : "#contact",
     __target : ""
   }
 //배열(array) 안에 obj(객체) 들어갔는가?
--- ndm/ndm.js
+++ ndm/ndm.js
@@ -41,6 +41,8 @@ window.addEventListener("load", function () {
     // requestAnimationFrame을 사용하여 성능 최적화
     requestAnimationFrame(handleScroll);
   });
-  document.getElementById('toggleFormBtn').onclick = () => document.getElementById('overlay').style.display = 'flex';
-  document.getElementById('closeFormBtn').onclick = () => document.getElementById('overlay').style.display = 'none';
+  document.getElementById("toggleFormBtn").onclick = () =>
+    (document.getElementById("overlay").style.display = "flex");
+  document.getElementById("closeFormBtn").onclick = () =>
+    (document.getElementById("overlay").style.display = "none");
 });
